82496f6ce7c94f7fed63246263924b0475bd2572
Ken Wainaina <wainainaken@gmail.com>
2017-01-02 19:34:19 +0300

    aecca

---
+++ app/src/main/java/ke/co/stashare/aecca/Adapters/MainActivity.java
@@ -0,0 +1,15 @@
+package ke.co.stashare.aecca.Adapters;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ke.co.stashare.aecca.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
